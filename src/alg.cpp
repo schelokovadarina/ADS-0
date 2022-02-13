1ae5d8a713983cab68451e2cfb92fdb2f1810df7
schelokovadarina <99324863+schelokovadarina@users.noreply.github.com>
2022-02-13 21:13:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,12 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-while (a != 0 && b != 0) {
-if (a > b)
-a%= b;
-else
-b%= a;
-  }
-return a + b;
+     while ((a!= 0)&&(b!= 0)) {
+        if (a > b) {
+        a%=b;
+         } else {
+            b%=a;
+        }
+    }
+    return (a+b);
 }
